e333d581d0b483ecb150955872dfd804b9e6b700
iPadKID27 <husvainglory@hotmail.com>
2025-05-23 12:51:05 +0700

    add Chapter1

---
+++ Chapter1/src/Simple.java
@@ -0,0 +1,6 @@
+public class Simple {
+    public static void main(String[] args) {
+        System.out.println("Prohramming is great fun!");
+        System.out.println("COMPUTER Programs");
+    }
+}
